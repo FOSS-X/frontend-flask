d13d85814b03c2211b25e427af73c28258616f6b
Sanjitha <sanjitha.singh@gmail.com>
2020-05-13 22:05:05 +0530

    add create new db modal

--- app/base/forms.py
+++ app/base/forms.py
@@ -5,7 +5,7 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from flask_wtf import FlaskForm
-from wtforms import TextField, PasswordField
+from wtforms import TextField, PasswordField, SelectField
 from wtforms.validators import InputRequired, Email, DataRequired
 
 ## login and registration
@@ -24,3 +24,7 @@ class ResetPasswordForm(FlaskForm):
     email           = TextField('Email'        , id='email_create'    , validators=[DataRequired(), Email()])
     password        = PasswordField('Password' , id='pwd_create'      , validators=[DataRequired()])
     repeat_password = PasswordField('Repeat Password', id='rpwd_create'     , validators=[DataRequired()])
+
+class CreateDatabaseForm(FlaskForm):
+    database_type = SelectField(u'Database Type', choices=[('mysql',"MySQL"),("mongodb","MongoDB")], id="database_type", validators=[DataRequired()])
+    database_name=TextField('Database Name'     , id='database_name_create' , validators=[DataRequired()])
