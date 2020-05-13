bb815d73000ea779ef46d37f4e50504847f67704
Mohammed Ataaur Rahaman <ataago7@gmail.com>
2020-05-13 17:40:57 +0530

    Update forms.py

--- app/base/forms.py
+++ app/base/forms.py
@@ -15,6 +15,12 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', id='pwd_login'        , validators=[DataRequired()])
 
 class CreateAccountForm(FlaskForm):
-    username = TextField('Username'     , id='username_create' , validators=[DataRequired()])
-    email    = TextField('Email'        , id='email_create'    , validators=[DataRequired(), Email()])
-    password = PasswordField('Password' , id='pwd_create'      , validators=[DataRequired()])
+    username        = TextField('Username'     , id='username_create' , validators=[DataRequired()])
+    email           = TextField('Email'        , id='email_create'    , validators=[DataRequired(), Email()])
+    password        = PasswordField('Password' , id='pwd_create'      , validators=[DataRequired()])
+    repeat_password = PasswordField('Repeat Password', id='rpwd_create'     , validators=[DataRequired()])
+
+class ResetPasswordForm(FlaskForm):
+    email           = TextField('Email'        , id='email_create'    , validators=[DataRequired(), Email()])
+    password        = PasswordField('Password' , id='pwd_create'      , validators=[DataRequired()])
+    repeat_password = PasswordField('Repeat Password', id='rpwd_create'     , validators=[DataRequired()])
