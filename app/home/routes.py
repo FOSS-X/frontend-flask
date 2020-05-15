f1bcfb19e2359780eba2c856dcdb68894012becb
Mohammed Ataaur Rahaman <ataago7@gmail.com>
2020-05-15 11:07:22 +0530

    Update routes.py

--- app/home/routes.py
+++ app/home/routes.py
@@ -5,18 +5,27 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from app.home import blueprint
-from flask import render_template, redirect, url_for, session, request
+from flask import render_template, redirect, url_for, session, request, flash
 # from flask_login import login_required, current_user
 # from app import login_manager
 from jinja2 import TemplateNotFound
+from app.base.util import *
 from app.base.forms import CreateDatabaseForm, UpdateEntitySetForm
 import requests
+import json
 
-
-UDAPI_URL = "http://localhost:2020"
-
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 @blueprint.route('/index', methods=['GET', 'POST'])
+@verify_user
 def index():
     # Initializing all forms for rendering
     createDbForm = CreateDatabaseForm()
@@ -32,7 +41,7 @@ def index():
         session['count_SQL'] = len(databasesResp['mysql'])
         session['count_noSQL'] = len(databasesResp['mongodb'])
     else:
-        print(user_data['message'])
+        print(databasesResp['message'])
         return render_template('errors/page_500.html'), 500
 
     # Retrive all EntitySets of each Databases
@@ -55,15 +64,114 @@ def index():
                 print(user_data['message'])
                 return render_template('errors/page_500.html'), 500
 
-    # databases = [{"name": "test-student", "type": "mysql", "entitySets": ['professors', 'lizards', 'mimosas']}, {"name": "test2", "type": "mongo", "entitySets": ['swords', 'reverse']}, {"name": "test3", "type": "mongo", "entitySets": ["Lorem", "ipsum", "dolor"]},
-    #              {"name": "test-student", "type": "mysql", "entitySets": ['professors', 'lizards', 'mimosas']}, {"name": "test2", "type": "mongo", "entitySets": ['bread', 'swords']}, {"name": "test3", "type": "mongo", "entitySets": ["Lorem", "ipsum", "dolor"]}]
-
-    # Creating Database Form
-    create_DB_form = CreateDatabaseForm(request.form)
+    # Creating new Database by sending request to UDAPI
     if 'createDB' in request.form:
-        return "Create     DB"
+        database_type = request.form['database_type']
+        database_name = request.form['database_name']
 
-    return render_template('index.html', databases=databases, createDbForm=create_DB_form, updateESForm=updateESForm)
+        url = UDAPI_URL + "/" + database_type + "/databases"
+        payload = {"databaseName":database_name}
+        headers = {'jwtToken': session['jwtToken']}
+        response = requests.post(url, headers=headers, json=payload)
+        data = response.json()
+
+        if data['success']:
+            flash(data["message"], "success")
+            return redirect(url_for('home_blueprint.index'))
+        elif not data['success']:
+            flash("You can't have two databases with the same name and type.", "danger")
+            return redirect(url_for('home_blueprint.index'))
+
+        print(data['message'])
+        return render_template('errors/page_500.html'), 500
+
+    # Deleteing a Database
+    if 'deleteDB' in request.form:
+        database_type = request.args.get('databaseType')
+        database_name = request.args.get('databaseName')
+
+        url = UDAPI_URL + "/" + database_type + "/databases/" + database_name
+        headers = {'jwtToken': session['jwtToken']}
+        response = requests.delete(url, headers=headers)
+        data = response.json()
+
+        if data['success']:
+            flash(data['message'], "success")
+            return redirect(url_for('home_blueprint.index'))
+        elif not data['success']:
+            flash(data['message'], "danger")
+            return redirect(url_for('home_blueprint.index'))
+        print(data['message'])
+        return render_template('errors/page_500.html'), 500
+ 
+    # Create a new Enity Set
+    if 'createEntitySet' in request.form:
+        database_type = request.args.get('databaseType')
+        database_name = request.args.get('databaseName')
+        entitySetName = request.form['entitySetName']
+        attributes_data = request.form['create-es-attributes']
+
+        payload = json.loads(attributes_data)
+        url = UDAPI_URL + "/" + database_type + "/databases/" + database_name
+        headers = {'jwtToken': session['jwtToken']}
+        response = requests.post(url, headers=headers, json=payload)
+        data = response.json()
+
+        if data['success']:
+            flash(data["message"], "success")
+            return redirect(url_for('home_blueprint.index'))
+        elif not data['success']:
+            flash(data["message"], "danger")
+            return redirect(url_for('home_blueprint.index'))
+
+        print(data['message'])
+        return render_template('errors/page_500.html'), 500
+
+    # rename an existing entity Set
+    if 'updateEntitySet' in request.form:
+        database_type = request.args.get('databaseType')
+        database_name = request.args.get('databaseName')
+        entitySetName = request.args.get('entitySetName')
+        newEntitySetName = request.form['entitySet_name']
+
+        url = UDAPI_URL + "/" + database_type + "/databases/" + database_name + "/" + entitySetName
+        headers = {'jwtToken': session['jwtToken']}
+        payload = {"newEntitySetName": newEntitySetName}
+        response = requests.put(url, headers=headers, json=payload)
+        data = response.json()
+
+        if data['success']:
+            flash(f"From {database_type} database: '{database_name}', Entity Set Renamed from:      {entitySetName} --> {newEntitySetName}", "success")
+            return redirect(url_for('home_blueprint.index'))
+        elif not data['success']:
+            flash(data["message"], "danger")
+            return redirect(url_for('home_blueprint.index'))
+
+        print(data['message'])
+        return render_template('errors/page_500.html'), 500
+
+    # Delete Enity Set
+    if 'deleteEntitySet' in request.form:
+        database_type = request.args.get('databaseType')
+        database_name = request.args.get('databaseName')
+        entitySetName = request.args.get('entitySetName')
+
+        url = UDAPI_URL + "/" + database_type + "/databases/" + database_name + "/" + entitySetName
+        headers = {'jwtToken': session['jwtToken']}
+        response = requests.delete(url, headers=headers)
+        data = response.json()
+        
+        if data['success']:
+            flash(f"From {database_type} database: '{database_name}', Entity Set: '{entitySetName}' deleted successfully", "success")
+            return redirect(url_for('home_blueprint.index'))
+        elif not data['success']:
+            flash(data["message"], "danger")
+            return redirect(url_for('home_blueprint.index'))
+
+        print(data['message'])
+        return render_template('errors/page_500.html'), 500
+
+    return render_template('index.html', databases=databases, createDbForm=createDbForm, updateESForm=updateESForm)
 
 # @blueprint.route('/index', methods=['GET', 'POST'])
 # def index():
@@ -74,58 +182,168 @@ def index():
 #     return render_template('index.html', databases=databases, createDbForm=createDbForm, updateESForm=updateESForm)
 
 
-@blueprint.route('/entity_sets/<databaseName>/<entitySetName>', methods=['GET'])
-def displayES(databaseName,entitySetName):
+@blueprint.route('/entity_sets/<databaseType>/<databaseName>/<entitySetName>', methods=['GET', 'POST'])
+@verify_user
+def displayES(databaseType,databaseName,entitySetName):
     updateESForm = UpdateEntitySetForm()
 
     #get the following data from API in this format
-    database = {"name": "Test", "type": "MongoDB",
-                "entitySets": ["uno", "dos", "tres", "four", "five"]}
-    
+    url = UDAPI_URL + "/" + databaseType + "/databases/" + databaseName 
+    headers = {'jwtToken': session['jwtToken']}
+    response = requests.get(url, headers=headers)
+    entitySetsData = response.json()
 
-    #by default get the first entityset to display in table 2
-    if entitySetName == None:
-        entitySet = {
-            "name": "uno",
-            "records": [
-                {"id": "ipsum", "name": "amet", "course": "captain"},
-                {"id": "morrison", "name": "frog", "course": "dinner"}
-            ],
-            "schema": {"id": "integer", "name": "string", "course": "string"},
-            "primary_key": "id"
+    if entitySetsData['success']:
+        entitySets = entitySetsData['entitySets']
+        database = {
+            "name":databaseName,
+            "type":databaseType,
+            "entitySets":entitySets
         }
+
     else:
-        entitySet = {
-            "name": "dos",
-            "records": [
-                {"id": "ipsum", "name": "amet", "course": "captain"},
-                {"id": "morrison", "name": "frog", "course": "dinner"}
-            ],
-            "schema": {"id": "integer", "name": "string", "course": "string"},
-            "primary_key": "id"
-        }
+        print(f"{bcolors.FAIL}Exception from UDAPI server side: {entitySetsData['message']}{bcolors.ENDC}")
+        return render_template('errors/page_500.html'), 500
+
+    # database = {"name": "Test", "type": "MongoDB",
+    #             "entitySets": ["uno", "dos", "tres", "four", "five"]}
+    
+    # Create a new Enity Set
+    if 'createEntitySet' in request.form:
+        database_type = request.args.get('databaseType')
+        database_name = request.args.get('databaseName')
+        entitySetName = request.form['entitySetName']
+        attributes_data = request.form['create-es-attributes']
+
+        payload = json.loads(attributes_data)
+        url = UDAPI_URL + "/" + database_type + "/databases/" + database_name
+        headers = {'jwtToken': session['jwtToken']}
+        response = requests.post(url, headers=headers, json=payload)
+        data = response.json()
+
+        if data['success']:
+            flash(data["message"], "success")
+            return redirect(url_for('home_blueprint.displayES', databaseType=databaseType, databaseName=databaseName, entitySetName="None"))
+        elif not data['success']:
+            flash(data["message"], "danger")
+            return redirect(url_for('home_blueprint.displayES', databaseType=databaseType, databaseName=databaseName, entitySetName="None"))
+
+        print(data['message'])
+        return render_template('errors/page_500.html'), 500
+
+    
+    # rename an existing entity Set
+    if 'updateEntitySet' in request.form:
+        database_type = request.args.get('databaseType')
+        database_name = request.args.get('databaseName')
+        entitySetName = request.args.get('entitySetName')
+        newEntitySetName = request.form['entitySet_name']
+
+        url = UDAPI_URL + "/" + database_type + "/databases/" + database_name + "/" + entitySetName
+        headers = {'jwtToken': session['jwtToken']}
+        payload = {"newEntitySetName": newEntitySetName}
+        response = requests.put(url, headers=headers, json=payload)
+        data = response.json()
+
+        if data['success']:
+            flash(f"From {database_type} database: '{database_name}', Entity Set Renamed from:      {entitySetName} --> {newEntitySetName}", "success")
+            return redirect(url_for('home_blueprint.displayES', databaseType=databaseType, databaseName=databaseName, entitySetName="None"))
+        elif not data['success']:
+            flash(data["message"], "danger")
+            return redirect(url_for('home_blueprint.displayES', databaseType=databaseType, databaseName=databaseName, entitySetName="None"))
+
+        print(data['message'])
+        return render_template('errors/page_500.html'), 500
+
+    # Delete Enity Set
+    if 'deleteEntitySet' in request.form:
+        database_type = request.args.get('databaseType')
+        database_name = request.args.get('databaseName')
+        entitySetName = request.args.get('entitySetName')
+
+        url = UDAPI_URL + "/" + database_type + "/databases/" + database_name + "/" + entitySetName
+        headers = {'jwtToken': session['jwtToken']}
+        response = requests.delete(url, headers=headers)
+        data = response.json()
+        
+        if data['success']:
+            flash(f"From {database_type} database: '{database_name}', Entity Set: '{entitySetName}' deleted successfully", "success")
+            return redirect(url_for('home_blueprint.displayES', databaseType=databaseType, databaseName=databaseName, entitySetName="None"))
+        elif not data['success']:
+            flash(data["message"], "danger")
+            return redirect(url_for('home_blueprint.displayES', databaseType=databaseType, databaseName=databaseName, entitySetName="None"))
+
+        print(data['message'])
+        return render_template('errors/page_500.html'), 500
+
+    # Get entities for the table
+    # If entityset selected is not None and is in entity Sets of the DatabaseName
+    if entitySetName in entitySets:
+        url = UDAPI_URL + "/" + databaseType + "/databases/" + databaseName + "/" + entitySetName
+        headers = {'jwtToken': session['jwtToken']}
+        response = requests.get(url, headers=headers)
+        entitiesData = response.json()
+
+        if entitiesData['success']:
+            entites = entitiesData['message']
+            if entites:
+                attributes = []
+                for i in entites[0].keys():
+                    attributes.append(i)
+                fieldTypes = [""] * len(attributes)
+                schema = dict(zip(attributes, fieldTypes))
+                primary_key = attributes[0]
+
+                entities = {
+                    "name":entitySetName,
+                    "records":entites,
+                    "schema":schema,
+                    "primary_key":primary_key
+                }
+            else:
+                entities = None
+                
+        elif not entitiesData['success']:
+            flash(entitiesData["message"], "danger")
+            return redirect(url_for('home_blueprint.displayES', databaseType=databaseType, databaseName=databaseName, entitySetName="None"))
+    else:
+        entities = None
+
+    
+    # if 'createEntity' in request.form:
+        # return entitiesData
+    
+    # if entitySetName == "None":
+    #     entities = {
+    #         "name": "uno",
+    #         "records": [
+    #             {"id": "ipsum", "name": "amet", "course": "captain"},
+    #             {"id": "morrison", "name": "frog", "course": "dinner"}
+    #         ],
+    #         "schema": {"id": "integer", "name": "string", "course": "string"},
+    #         "primary_key": "id"
+    #     }
+    # else:
+    #     entities = {
+    #         "name": "dos",
+    #         "records": [
+    #             {"id": "ipsum", "name": "amet", "course": "captain","courses": "captaisn"},
+    #             {"id": "morrison", "name": "frog", "course": "dinner"}
+    #         ],
+    #         "schema": {"id": "integer", "name": "string", "course": "string"},
+    #         "primary_key": "id"
+    #     }
 
     try:
-        return render_template('entity_sets.html', database=database, entitySet=entitySet, updateESForm=updateESForm)
+        return render_template('entity_sets.html', database=database, entitySet=entities, updateESForm=updateESForm)
 
     except TemplateNotFound:
         return render_template('error-404.html'), 404
 
-    # except:
-    #     return render_template('error-500.html'), 500
-
 
 @blueprint.route('/<template>')
 def route_template(template):
-
-    # if not "username" in session:
-    #     return redirect(url_for('base_blueprint.login'))
-
-    # if not current_user.is_authenticated:
-    #     return redirect(url_for('base_blueprint.login'))
-    # return render_template(template + '.html')
     try:
-
         return render_template(template + '.html')
 
     except TemplateNotFound:
@@ -133,8 +351,3 @@ def route_template(template):
 
     except:
         return render_template('error-500.html'), 500
-
-
-@blueprint.app_errorhandler(404)
-def handle_exceptions(e):
-    return render_template('error-404.html'), 404
